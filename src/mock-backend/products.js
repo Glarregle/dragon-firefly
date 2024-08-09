c5c3fe3de4fbb151831f7d5c5b20a211f7921a70
Gabriela <gabilarreglemail@gmail.com>
2024-08-09 11:29:27 +0200

    chore: mock data

---
+++ src/mock-backend/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    name: 'Lavender Perfume',
+    price: 29.99,
+    image: 'https://dummyjson.com/image/150',
+    description: 'A soothing blend of lavender and other essential oils.'
+  },
+  {
+    id: 2,
+    name: 'Citrus Splash',
+    price: 34.99,
+    image: 'https://dummyjson.com/image/150',
+    description: 'A refreshing citrus fragrance with a hint of mint.'
+  },
+  // Add more products as needed
+];
+
+export default products;
