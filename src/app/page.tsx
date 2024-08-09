53b303e5f15de695074156b5e5a0c1975b70a15a
Gabriela <gabilarreglemail@gmail.com>
2024-08-09 14:55:21 +0200

    fix: add unique identifier in list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div>
       <h1>Perfume Collection</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {products.map((product) => (
-          <div key={product.id} style={{ margin: '20px', width: '200px' }}>
+        {products.map((product, index) => (
+          <div key={index} style={{ margin: '20px', width: '200px' }}>
             <img src='https://dummyjson.com/image/150' alt={product.TITLE} style={{ width: '100%' }} />
             <h2>{product.TITLE}, {product.BRAND}</h2>
             <p>{product.VOLUME}</p>
