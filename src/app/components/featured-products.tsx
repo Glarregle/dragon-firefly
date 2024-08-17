0a9a20fad6927276e6a5be5bc628a000efe19d3d
Gabriela <gabilarreglemail@gmail.com>
2024-08-17 17:36:24 +0200

    feat: enable "featured" perfumes API, update type, update Home and fetch

--- src/app/components/featured-products.tsx
+++ src/app/components/featured-products.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import products from '../../mock-backend/products';
 import { Product } from '../types'
 
 interface FeaturedProductsProps {
@@ -10,25 +9,18 @@ const FeaturedProducts = async ({ children } : FeaturedProductsProps) => {
 
   // GET featured products
 
-  // function to fetch featured products
-  // or create a featured products list component that requests these, via server rendering
   // generate static segments for the featured products, so is server side
-  // perfume product `featured: boolean = true`
-  // fetch featured featured API response with three products
   // then generate segments with prodcuts ids
 
-
-  // Fetch data in the server component
-  // const fetchFeaturedProducts = async () => {
-  //   const res = await fetch('https://api.example.com/products?featured=true');
-  //   const data: Product[] = await res.json();
-  //   return data;
-  // };
-
-  // const products = fetchFeaturedProducts();
-
-  const featuredProducts = [products[0], products[1], products[2]];
+  const res = await fetch('http://localhost:3000/api/perfumes?featured=true', { cache: 'no-store' });
   
+  if (!res.ok) {
+    console.log('Failed to fetch data:', res);
+    return <div>Error fetching products.</div>;
+  }
+  
+  const { data } = await res.json();
+  const featuredProducts: Product[] = data;
 
   return (
 	  <div className="container mx-auto px-4 py-8">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,34 @@
 import FeaturedProduct from './components/featured-product';
-import Link from 'next/link'
-import products from '../mock-backend/products';
 import FeaturedProducts from './components/featured-products';
+import Link from 'next/link'
+import { Product } from './types'
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch('http://localhost:3000/api/perfumes');
+  if (!res.ok) {
+    console.log('Failed to fetch data:', res);
+    return <div>Error fetching products.</div>;
+  }
+
+  const { data }: { data: Product[] } = await res.json();
+  const products = data;
 
   return (
     <div className='flex flex-col flex-auto'>
       <h1>Perfume Collection</h1>
+
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {products.slice(0, 4).map((product, index) => (
-          <div key={index} style={{ margin: '20px', width: '200px' }}>
-            <img src='https://dummyjson.com/image/150' alt={product.TITLE} style={{ width: '100%' }} />
-            <h2>{product.TITLE}, {product.BRAND}</h2>
-            <p>{product.VOLUME}</p>
-            <p>${product.PRICE}</p>
+        {products.slice(0, 4).map((product: Product) => (
+          <div key={product.id} style={{ margin: '20px', width: '200px' }}>
+            <img src='https://dummyjson.com/image/150' alt={product.title} style={{ width: '100%' }} />
+            <h2>{product.title}, {product.brand}</h2>
+            <p>{product.volume}</p>
+            <p>${product.price}</p>
           </div>
         ))}
       </div>
+
       <h2 className='text-2xl mb-4'>Featured products</h2>
 
       {/* Composition over inheritance approach */}
